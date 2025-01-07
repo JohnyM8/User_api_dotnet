85c0bc428c15f35dd7bd5e6fec07accc7e5b7486
janek <janek@LAPTOP-N1HDEGUP>
2025-01-07 12:00:29 +0100

    Make things looks nicer

---
+++ WebApplication1/Models/Logins.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication1.Models
+{
+    public class LoginCredential
+    {
+        public string? login { get; set; }
+        public string? password { get; set; }
+    }
+
+    public class LoginResponseFromDto
+    {
+        public string? token { get; set; }
+    }
+
+    public class LoginToDto
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+}
