68808bdb4502eb742faf71466d0b886bdf719e8a
Janek <Janek@JANEK>
2025-01-15 00:06:33 +0100

    ch

--- WebApplication1/Controllers/UsersController.cs
+++ WebApplication1/Controllers/UsersController.cs
@@ -126,6 +126,24 @@ namespace WebApplication1.Controllers
             });
         }
 
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserToFront>> GetUsers(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return new UserToFront(user);
+        }
+
+
+
+
+
         //GARBAGE ONLY BELOW//
 
 
@@ -280,19 +298,7 @@ namespace WebApplication1.Controllers
         }
 
         // GET: api/Users/5
-        [Authorize]
-        [HttpGet("{id}")]
-        public async Task<ActionResult<UserToFront>> GetUsers(int id)
-        {
-            var user = await _context.Users.FindAsync(id);
-
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return new UserToFront(user);
-        }
+        
 
         // PUT: api/Users/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
